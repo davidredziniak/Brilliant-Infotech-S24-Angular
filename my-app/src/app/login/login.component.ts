e83a75be508201d4e62be5576450d933d726b2aa
davidredziniak <redziniakdavid@gmail.com>
2024-08-08 12:35:14 -0400

    Configure User details component form styling and submission validation

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -16,13 +16,18 @@ export class LoginComponent {
     // Require form group to have username and password filled
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
-      password: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
   // Function that is called when user submits the form
   onSubmit() {
     const { username, password } = this.loginForm.value;
+
+    // Check if any fields are empty
+    if (username === "" || password === ""){
+      alert("All fields must be filled out.");
+    }
 
     // Check if user is not authenticated
     if (!this.auth.login(username, password)) {
--- my-app/src/app/user-details/user-details.component.ts
+++ my-app/src/app/user-details/user-details.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css',
 })
-export class UserDetailsComponent {}
+export class UserDetailsComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Require form group to have all input fields
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      addressOne: ['', Validators.required],
+      addressTwo: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required],
+      country: ['', Validators.required]
+    });
+  }
+
+  // Function that is called when user submits the form
+  onSubmit() {
+    const { firstName, lastName, email, phoneNumber, addressOne, addressTwo, city, state, zip, country } = this.contactForm.value;
+
+    // Check if any required fields are empty
+    if (firstName === "" || lastName === "" || email === "" || phoneNumber === "" || addressOne === "" || city === "" || state === "" || zip === "" || country === ""){
+      alert("All fields must be filled out.");
+    }
+    
+
+  }
+}
