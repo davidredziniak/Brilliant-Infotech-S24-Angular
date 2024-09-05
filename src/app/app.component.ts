40fde8850218f389883c60fdffcb0087708a2c4b
davidredziniak <redziniakdavid@gmail.com>
2024-09-05 18:41:40 -0400

    Add animations for login and register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { animate, state, style, transition, trigger } from "@angular/animations";
-import { isPlatformBrowser } from "@angular/common";
-import { Component, Inject, PLATFORM_ID } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component } from "@angular/core";
+import { ChildrenOutletContexts, RouterOutlet } from "@angular/router";
+import { slideInAnimation } from "./animations";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css"
+  styleUrl: "./app.component.css",
+  animations: [
+    slideInAnimation
+  ]
 })
+
 
 export class AppComponent {
   title = "my-app";
+
+  constructor(private contexts: ChildrenOutletContexts) {}
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,13 @@ export const routes: Routes = [
     path: "login",
     loadComponent: () =>
       import("./login/login.component").then((m) => m.LoginComponent),
+    data: { animation: 'One' }
   },
   {
     path: "register",
     loadComponent: () =>
       import("./register/register.component").then((m) => m.RegisterComponent),
+    data: { animation: 'Two' }
   },
   {
     path: "user",
