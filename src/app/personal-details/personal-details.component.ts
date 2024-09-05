6337483d75d9eedf092af51c134ac4e7b11624f7
davidredziniak <redziniakdavid@gmail.com>
2024-09-05 19:04:09 -0400

    Convert JS alerts to bootstrap alerts using NgbAlert

--- src/app/personal-details/personal-details.component.ts
+++ src/app/personal-details/personal-details.component.ts
@@ -4,15 +4,23 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
 import { UserService } from '../_services/user.service';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+
+interface Alert {
+	type: string;
+	message: string;
+}
 @Component({
   selector: 'app-personal-details',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgbAlert],
   templateUrl: './personal-details.component.html',
   styleUrl: './personal-details.component.css'
 })
 export class PersonalDetailsComponent {
   personalForm: FormGroup;
+  alerts: Alert[] = [];
+
   private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient, private fb: FormBuilder, private router: Router, private user: UserService) {
@@ -27,22 +35,33 @@ export class PersonalDetailsComponent {
 
   }
 
+  close(alert: Alert) {
+		this.alerts.splice(this.alerts.indexOf(alert), 1);
+	}
+
   // Function that is called when user submits the form
   onSubmit() {
     const { occupation, hobbies, visited, artist, musician } = this.personalForm.value;
 
     // Check if any required fields are empty
     if (occupation === "" || hobbies === "" || visited === "" || artist === "" || musician === ""){
-      alert("All required fields must be filled out.");
+      let alert: Alert = {
+        type: 'warning',
+        message: 'All required fields must be filled out.'
+      };
+      this.alerts.push(alert);
     } else {
       // API call to submit personal details for user
       this.http.post<any>(this.apiUrl + "/personal", { occupation, hobbies, visited, artist, musician }).subscribe({
-        next: res => {
-          alert(res.message);
+        next: () => {
           this.router.navigate(['/home']);
         },
         error: err => {
-          alert(err.error.message);
+          let alert: Alert = {
+            type: 'danger',
+            message: err.error.message
+          };
+          this.alerts.push(alert);
         }
       });
     }
--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -4,16 +4,24 @@ import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { UserService } from '../_services/user.service';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 
+interface Alert {
+	type: string;
+	message: string;
+}
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgbAlert],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css',
 })
+
 export class UserDetailsComponent {
   contactForm: FormGroup;
+  alerts: Alert[] = [];
+
   private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient, private fb: FormBuilder, private router: Router, private user: UserService) {
@@ -32,23 +40,34 @@ export class UserDetailsComponent {
     });
   }
 
+  close(alert: Alert) {
+		this.alerts.splice(this.alerts.indexOf(alert), 1);
+	}
+
   // Called when user submits the form
   onSubmit() {
     const { firstName, lastName, email, phoneNumber, addressOne, addressTwo, city, state, zip, country } = this.contactForm.value;
 
     // Check if any required fields are empty
     if (firstName === "" || lastName === "" || email === "" || phoneNumber === "" || addressOne === "" || city === "" || state === "" || zip === "" || country === ""){
-      alert("All required fields must be filled out.");
+      let alert: Alert = {
+        type: 'warning',
+        message: 'All required fields must be filled out.'
+      };
+      this.alerts.push(alert);
     } else {
       
       // API call to submit user details
       this.http.post<any>(this.apiUrl + "/user", { firstName, lastName, email, phoneNumber, addressOne, addressTwo, city, state, zip, country }).subscribe({
-        next: res => {
-          alert(res.message);
+        next: () => {
           this.router.navigate(['/personal']);
         },
         error: err => {
-          alert(err.error.message);
+          let alert: Alert = {
+            type: 'danger',
+            message: err.error.message
+          };
+          this.alerts.push(alert);
         }
       });
     }
